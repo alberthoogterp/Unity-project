56fa0718c4acc4e2697b85b5beca5c136d61ef5e
albert <albert_dino@live.nl>
2023-04-19 15:58:24 +0200

    change

---
+++ Assets/physicsObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class physicsObject : MonoBehaviour
+{
+    public bool Grabbable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grabbable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/playerControlls.cs
+++ Assets/playerControlls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playerControlls : MonoBehaviour
 {
     public bool onTerrain;
+    private GameObject mouseOverObject; 
     private float maxRange = 30.0f;
     private Camera mainCamera;
     // Start is called before the first frame update
@@ -20,25 +21,35 @@ public class playerControlls : MonoBehaviour
         if(!Input.GetMouseButton(0) || Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) || !onTerrain){
             FollowMouse();
         }
+        if(Input.GetMouseButtonDown(1)){
+            Interract();
+        }
     }
 
     private void FollowMouse(){
-        mouseX
-        /*Vector3 nextPosition = this.gameObject.transform.position;
-        RaycastHit midRayHitInfo;
-        GameObject mouseOverObject;
+        Vector3 nextPosition = this.gameObject.transform.position;
+        RaycastHit rayHitInfo;
         Vector3 mouseOverLocation;
 
-        if(Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out midRayHitInfo) && midRayHitInfo.distance < maxRange){
+        if(Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out rayHitInfo) && rayHitInfo.distance < maxRange){
             onTerrain = true;
-            mouseOverObject = midRayHitInfo.collider.gameObject;
-            mouseOverLocation = midRayHitInfo.point;
+            mouseOverObject = rayHitInfo.collider.gameObject;
+            mouseOverLocation = rayHitInfo.point;
             nextPosition = mouseOverLocation;
         }
         else{
             onTerrain = false;
             nextPosition = mainCamera.ScreenPointToRay(Input.mousePosition).GetPoint(maxRange);
+            mouseOverObject = null;
         }   
-        transform.position = nextPosition;*/
+        transform.position = nextPosition;
+    }
+
+    private void Interract(){
+        if(mouseOverObject != null ){
+            if(mouseOverObject.GetComponent<physicsObject>().Grabbable){
+                Debug.Log(mouseOverObject.name);
+            }
+        }
     }
 }
